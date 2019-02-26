d068c33a1809ddfc8b46d683b2c99ee181e7cca8
Vigoxin <vigoxin@gmail.com>
2019-02-26 20:06:10 +0000

    added 2 images to canterbury urovasc to show both libreoffice and ms excel copy and paste

--- static/js/home/imgModal.js
+++ static/js/home/imgModal.js
@@ -17,6 +17,14 @@ if ($('#myModal').length) {
 	    modalImg.src = this.src;
 	}
 
+	var img2 = $('.rota-screenshot')[1];
+	var modalImg = document.getElementById("img01");
+	img2.onclick = function(){
+	    modal.style.display = "block";
+	    modalImg.src = this.src;
+	}
+
+
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
 
@@ -32,3 +40,38 @@ if ($('#myModal').length) {
 	});
 
 }
+
+// if ($('#myModal2').length) {
+// 	function changeImage() {
+// 		hospital = niceToNasty($('.dropdown.hospital').val());
+// 		specialty = niceToNasty($('.dropdown.specialty').val());
+// 		imgURL = rotas[hospital][specialty]['img'];
+// 		$('.rota-screenshot').attr('src', imgURL);
+// 	}
+
+// 	// Get the modal
+// 	var modal = document.getElementById('myModal');
+
+// 	// Get the image and insert it inside the modal - use its "alt" text as a caption
+// 	var img = $('.rota-screenshot')[1];
+// 	var modalImg = document.getElementById("img02");
+// 	img.onclick = function(){
+// 	    modal.style.display = "block";
+// 	    modalImg.src = this.src;
+// 	}
+
+// 	// Get the <span> element that closes the modal
+// 	var span = document.getElementsByClassName("close")[0];
+
+// 	// When the user clicks on <span> (x), close the modal
+// 	span.onclick = function() { 
+// 	  modal.style.display = "none";
+// 	}
+
+// 	$(window).click(function() {
+// 		if (event.target == modal) {
+// 			modal.style.display = "none";
+// 		}
+// 	});
+
+// }
